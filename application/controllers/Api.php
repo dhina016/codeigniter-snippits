aee63d272270c8b4bfc8a15bcaac00196508a378
Dinesh <46128375+dhina016@users.noreply.github.com>
2021-06-30 15:25:39 +0530

    Update Api.php

--- application/controllers/Api.php
+++ application/controllers/Api.php
@@ -5,11 +5,18 @@ class Api extends CI_Controller {
 
     public function __construct()
 	{
-		parent::__construct();
-
+	parent::__construct();
         $this->db->db_select(DATABASE_NAME);
         $this->load->model('basemodel');
-	} 
+	}
+	
+    function json_output($statusHeader, $response)
+    	{
+        $ci = &get_instance();
+        $ci->output->set_content_type('application/json');
+        $ci->output->set_status_header($statusHeader);
+        $ci->output->set_output(json_encode($response));
+    	}
 
 	public function index()
 	{
