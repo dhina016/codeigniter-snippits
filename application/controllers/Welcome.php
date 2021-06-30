f5239f3e94a66b9316926785e944e9c2222835f2
Dinesh <46128375+dhina016@users.noreply.github.com>
2021-06-30 15:29:19 +0530

    Update Welcome.php

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -24,4 +24,26 @@ class Welcome extends CI_Controller {
         $status = json_decode($output, true);
         return $status;
     	}
+	
+	function sendCustomMail($email, $text, $subject)
+    	{
+        $config = array(
+            'protocol' => 'smtp',
+            'smtp_host' => 'xxx',
+            'smtp_port' => 587,
+            'smtp_user' => 'xxx',
+            'smtp_pass' => 'xxx',
+            'mailtype' => 'html',
+            'charset' => 'iso-8859-1',
+            'wordwrap' => TRUE
+        );
+        $this->load->library('email', $config);
+        $this->email->set_mailtype("html");
+        $this->email->set_newline("\r\n");
+        $this->email->from('no-reply@hackerji.com', 'Team Hackerji');
+        $this->email->to($email);
+        $this->email->subject($subject);
+        $this->email->message($text);
+        $this->email->send();
+    }
 }
